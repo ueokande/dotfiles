1fdb70ab7ec175c4b57ace8c7e1f5aa40337063f
Shin'ya Ueoka <ueokande@i-beam.org>
2017-02-11 22:44:58 +0900

    "o" and "O" as vimperator

--- .config/vimfx/config.js
+++ .config/vimfx/config.js
@@ -1,9 +1,10 @@
 let {commands} = vimfx.modes.normal;
+
+// tabs
 vimfx.addCommand({
   name: 'search_tabs',
   description: 'Search tabs',
   category: 'tabs',
-  order: commands.focus_location_bar.order + 1,
 }, ({vim}) => {
   let {gURLBar} = vim.window;
   commands.focus_location_bar.run({vim});
@@ -11,6 +12,34 @@ vimfx.addCommand({
   gURLBar.onInput(new vim.window.KeyboardEvent('input'));
 });
 vimfx.set('custom.mode.normal.search_tabs', 'b');
+
+// location
+vimfx.addCommand({
+  name: 'open_url',
+  description: 'Open a URL',
+  category: 'location',
+  order: commands.focus_location_bar.order,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  commands.focus_location_bar.run({vim});
+  gURLBar.value = '';
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.open_url', 'o');
+
+vimfx.addCommand({
+  name: 'alter_url',
+  description: 'Open a URL based on current location',
+  category: 'location',
+  order: commands.focus_location_bar.order + 1,
+}, ({vim}) => {
+  let {gURLBar} = vim.window;
+  let length = gURLBar.value.length;
+  commands.focus_location_bar.run({vim});
+  gURLBar.setSelectionRange(length, length)
+  gURLBar.onInput(new vim.window.KeyboardEvent('input'));
+});
+vimfx.set('custom.mode.normal.alter_url', 'O');
 
 vimfx.addCommand({
   name: 'zoom_enlarge',
